0bf26e1c168f84d66e43e0eeef66441c22b55dee
dbsgmlrua <dbsgmlrua2@gmail.com>
2020-02-19 23:02:42 +0900

    20200219

---
+++ lesson4/app.js
@@ -0,0 +1,15 @@
+const port = 3000,
+http = require("http"),
+httpStatus = require('http-status-code'),
+app = http.createServer((request, response) => {
+    console.log("Received an incoming request!");
+    response.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    let responseMessage = "<h1>Hello, Universe!</h1>";
+    response.write(responseMessage);
+    response.end();
+    console.log(`Sent a response : ${responseMessage}`);
+});
+app.listen(port);
+console.log(`Ther server has started and is listening on port number: ${port}`);
